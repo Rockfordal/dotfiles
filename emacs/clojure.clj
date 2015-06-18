c40a0487bdde92dc9524dbab7c2bb9fd6f6a0098
Anders Lundström <anders@urkraft.se>
2015-06-18 14:49:51 +0200

    clojure.clj gist

---
+++ emacs/clojure.clj
@@ -0,0 +1,7 @@
+
+; istället för att (ns :refresh)
+;https://github.com/clojure/tools.namespace
+;[org.clojure/tools.namespace "0.2.11-SNAPSHOT"]
+(require '[clojure.tools.namespace.repl :refer [refresh]])
+(refresh)
+
