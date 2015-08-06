322020ccc880eb8b3c9333e86f2deb2bf93e4d75
Anders Lundström <anders@urkraft.se>
2015-08-06 17:59:04 +0200

    just nu vill vi ha solarized

--- .emacs.d/conf/themes.el
+++ .emacs.d/conf/themes.el
@@ -12,7 +12,7 @@
 ;(load-theme 'tango t)
 ;(load-theme 'prius t)                   ;som jellybeans fast lite ljusare
 ;(load-theme 'tango-dark t)              ;lik jelllybeans
-(load-theme 'material t)
+;(load-theme 'material t)
 
 ;(require '~/.emacs.d/lib/color-themes/color-theme-jellybeans)
 ;(require '~/.emacs.d/lib/color-themes/color-theme-oceandeep)
@@ -21,5 +21,5 @@
 ;(color-theme-oceandeep)
 ;(color-theme-jellybeans)
 ;(color-theme-sanityinc-tomorrow-night)
-;(color-theme-sanityinc-solarized-dark)
+(color-theme-sanityinc-solarized-dark)
 ;(color-theme-vibrant-ink)
